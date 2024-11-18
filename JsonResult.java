9792176a3619753b1da6a61ad59d61f163f95d99
murraymeghan202 <murrayalex2024.5@gmail.com>
2024-11-18 11:49:29 -0500

    Add files via upload

---
+++ JsonResult.java
@@ -0,0 +1,12 @@
+package com.discord.tracker;
+
+public class JsonResult {
+    private String IP;
+    private String hostname;
+    private String type;
+    private String continent_code;
+    private String continent_name;
+    private String region_code;
+    private String region_name;
+    private String city;
+}
